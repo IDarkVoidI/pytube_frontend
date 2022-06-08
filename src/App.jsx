94064f485e19ef6aa5dfa8193affe79ad2abc2ca
Shah <shahmansurov1001@gmail.com>
2022-06-08 18:13:11 -0400

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,32 @@ import './App.css';
 import ChannelCard from './Components/Cards/ChannelCard';
 import Navbar from './Components/Navbar/Navbar';
 
+const data = [
+  {
+    img: "https://images.unsplash.com/photo-1654665462149-427b4cbc94ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    title: "Test1", id: "1", description: "description for test1"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1654616605705-2ef59f08ae66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    title: "Test2", id: "2", description: "description for test2"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1654616340351-9653c80c5f1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    title: "Test3", id: "3", description: "description for test3"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1654586443280-8db3df4704df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    title: "Test4", id: "4", description: "description for test4"
+  }
+]
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <hr />
-      <div>
-        <ChannelCard />
-        <ChannelCard />
-        <ChannelCard />
+      <div className='flex'>
+        {data.map((channel) => <ChannelCard img={channel.img} title={channel.title} key={channel.id} description={channel.description} />)}
       </div>
     </div>
   );
--- src/Components/Cards/ChannelCard.jsx
+++ src/Components/Cards/ChannelCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import './ChannelCard.css'
 
-const ChannelCard = () => {
+
+const ChannelCard = (props) => {
     return (
-        <div>
-            <img src='https://images.unsplash.com/photo-1654475677139-42da602e6d07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60' alt="channel_pfp" />
-            <h3>Sample Title</h3>
+        <div className="card">
+            <img src={props.img} alt="channel_pfp" />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
         </div>
     )
 }
