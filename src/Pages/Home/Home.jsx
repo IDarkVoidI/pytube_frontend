92b81716c9f8e1b043be9a52e7f7ee361656027a
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2022-06-23 23:41:44 +0300

    implement searching

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Input from "../../Components/Input/Input";
 import Lottie from "react-lottie";
 import animation from "../../lib/animation.json";
 import "./Home.css";
-import { BsSearch } from "react-icons/bs";
+import { BsSearch as SearchIcon } from "react-icons/bs";
+import axios from "axios";
 
 const animationOptions = {
   loop: true,
@@ -15,7 +16,21 @@ const animationOptions = {
   },
 };
 
+//TODO: randomize placeholder with random technology name
 function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // prevents page refresh on submiting data
+    const res = await axios.get(`http://localhost:8000/api/video?query=${searchTerm}`);
+    console.log(res.data);
+    return res.data;
+  };
+
   return (
     <div>
       <Navbar />
@@ -32,15 +47,15 @@ function Home() {
               <h3 className="h-2" style={{ margin: "0 0 10px 0", padding: "5px 0", fontSize: "2em" }}>
                 Search for tutorials
               </h3>
-              <div className="hero-section-input--container flex center-items">
-                <BsSearch />
+              <form onSubmit={handleSubmit} className="hero-section-input--container flex center-items">
+                <SearchIcon />
                 <Input
                   type="search"
-                  onChange={() => console.log("searching")}
+                  onChange={handleSearchChange}
                   placeholder="Django..."
                   className={"hero-search-input"}
                 />
-              </div>
+              </form>
             </div>
           </div>
         </div>
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-import VideoCard from '../../Components/Cards/Video/VideoCard'
+import React from "react";
+import { useState, useEffect } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import VideoCard from "../../Components/Cards/Video/VideoCard";
 
 const Video = () => {
-    const [videos, setVideos] = useState([])
+  const [videos, setVideos] = useState([]);
 
-    const fetchVideos = async () => {
-        const response = await fetch('http://localhost:8000/api/video')
-        const result = await response.json()
-        setVideos(result)
-    };
+  const fetchVideos = async () => {
+    const response = await fetch("http://localhost:8000/api/video");
+    const result = await response.json();
+    setVideos(result);
+  };
 
-    useEffect(() => {
-        fetchVideos()
-    }, [])
+  useEffect(() => {
+    fetchVideos();
+  }, []);
 
-    return (
-        <div>
-            <Navbar />
-            <div>
-                {videos.map((video) => <VideoCard link={video.source_link} title={video.title} channel={video.channel} />)}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <div>
+        {videos.map((video) => (
+          <VideoCard link={video.source_link} title={video.title} channel={video.channel} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Video
+export default Video;
