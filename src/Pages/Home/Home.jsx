b6f9b96569573dea23830eaa3bad409d991a9de6
Mukhammad Sobirov <mukhammadsobirov.ms@gmail.com>
2022-06-23 16:57:25 +0300

    implement lottie react

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import Input from "../../Components/Input/Input";
+import Lottie from "react-lottie";
+import animation from "../../lib/animation.json";
+import "./Home.css";
+
+const animationOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animation,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 
 function Home() {
   return (
     <div>
       <Navbar />
       <div className="container">
-        <h1 className="h-1">Typography</h1>
-        <h2 className="h-2">Typography</h2>
-        <h3 className="h-3">Typography</h3>
-        <p className="text">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae non vel consequatur officia repellat facilis
-          porro ex distinctio? Explicabo, optio id fugit tempore tenetur quod voluptatum ut impedit! Reprehenderit
-          eligendi eum consectetur officia eius beatae reiciendis ad, soluta molestiae aliquid. Consequatur itaque
-          deserunt porro dolores fuga ad, rem ullam excepturi! Vel qui cumque labore necessitatibus blanditiis. Dolorum
-          voluptate quaerat numquam praesentium nostrum ipsa sapiente alias provident. Laboriosam aperiam dignissimos
-          nostrum suscipit delectus, neque eius facilis dicta velit maxime doloribus repudiandae expedita obcaecati fuga
-          dolores illo ipsa, a atque hic! Voluptatibus eaque delectus iste odit corrupti magnam deleniti excepturi
-          doloribus suscipit?
-        </p>
-        <small className="small-text">Typography</small>
-        <br />
-        <a className="link" href="/">
-          Typography
-        </a>
+        <div className="hero">
+          <div className="hero-container flex center-items">
+            <div style={{ width: "45%" }}>
+              <Lottie width={"90%"} options={animationOptions} />
+            </div>
+            <div>
+              <h1 className="h-1" style={{ margin: 0, fontSize: "3.5em" }}>
+                Skill Repository
+              </h1>
+              <h3 className="h-2" style={{ margin: "0 0 10px 0", padding: "5px 0", fontSize: "2em" }}>
+                Search for tutorials
+              </h3>
+              <Input />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
